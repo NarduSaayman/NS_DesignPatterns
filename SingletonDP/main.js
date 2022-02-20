f554cf35d64ed99fa3694f59a95e3b0afa3b9765
NarduSaayman <nsaayman@jhb.dvt.co.za>
2022-02-20 19:34:47 +0200

    feat: :sparkles: Added database singleton example

--- SingletonDP/main.js
+++ SingletonDP/main.js
@@ -1,8 +1,10 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Singleton!</h1>
-`
+document.querySelector('#app').innerHTML = `<h1>Hello Singleton!</h1>`
+
+// Karazhan Ley Line Example
+document.querySelector('#app').innerHTML += `<h1>Karazhan Ley Line Example:<h1>`;
+
 function HailMedivhAtKarazhan(){
   let KarazhanInstance = null; // Karazhan is the big mage tower
 
@@ -33,10 +35,56 @@ function HailMedivhAtKarazhan(){
 }
 
 const Karazhan = HailMedivhAtKarazhan();
+console.log(`%c Karazhan Example`,`color: aqua; font-size:30px`);
 Karazhan.openLeyline(); // Open ley line #1
-Karazhan.openLeyline(); // Open ley line #2
-Karazhan.openLeyline(); // Open ley line #3
+Karazhan.openLeyline(); // Open ley line #1
+Karazhan.openLeyline(); // Open ley line #1
 Karazhan.closeLeyLine(); // Close ley line
-Karazhan.openLeyline(); // Open ley line #4
-Karazhan.openLeyline(); // Open ley line #5
+Karazhan.openLeyline(); // Open ley line #1
+Karazhan.openLeyline(); // Open ley line #1
 Karazhan.closeLeyLine(); // Close ley line again
+
+// Database Connection Example:
+document.querySelector('#app').innerHTML += `<h1> Database Example<h1>`;
+
+function DatabaseConnection(){
+  let DatabaseInstance = null; // Karazhan is the big mage tower
+
+  // track how many instances of Karazhan exists
+  let count = 0
+
+  function init(){
+    console.log(`Opening connection #${count + 1}`);
+    document.querySelector('#app').innerHTML += `<p>Opening connection #${count + 1}</p>`;
+    // now go and speak with Medivh about the horbile things befalling Azeroth
+  }
+
+  function createInstance(){
+    if(DatabaseInstance == null){
+      DatabaseInstance = init();
+    }
+    return DatabaseInstance;
+  }
+  function closeInstance(){
+    console.log(`Closing database connection`);
+    document.querySelector('#app').innerHTML += `<p>Closing database connection</p>`;
+    DatabaseInstance = null;
+  }
+  return{
+    open: createInstance,
+    close: closeInstance
+  }
+}
+
+const database = DatabaseConnection();
+console.log(`%c Database Example`,`color: red; font-size:30px`);
+database.open();
+database.open();
+database.close();
+database.open();
+database.close();
+database.open();
+database.open();
+database.open();
+database.open();
+database.close();
